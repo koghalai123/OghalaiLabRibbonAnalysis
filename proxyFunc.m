92fe7f5237bc50abe053432a49b6aa1264d2a033
koghalai123 <koghalai@gmail.com>
2020-08-02 20:44:35 -0700

    g

--- proxyFunc.m
+++ proxyFunc.m
@@ -1,4 +1,4 @@
 function proxyFunc(app)
-
+    %testing
     checkData3D(app,app.resizedAllData,str2double(app.CheckData3DDropDown.Value),app.newCenters,app.ribbon(1).grouped,app.ribbon(2).grouped,app.imageSize,[app.NChEditField.Value,app.PreChEditField.Value,app.PostChEditField.Value,app.LastChEditField.Value],app.resizedDimensions,app.resizedVoxel,app.dimensions);
 end
